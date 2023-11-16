ae16044bce8c4b25cbb219e92084207d6aa75583
zineb <zineb.laanaya@gmail.com>
2023-11-16 12:33:33 +0100

    creation model

---
+++ BACKEND/model/model.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+// Ajout plugin pour unique mail
+const muv = require('mongoose-unique-validator');
+
+
+const userSchema = mongoose.Schema({
+    firstName :{type:String,required:true},
+    lastName :{type:String,required:true},
+    email :{type:String,required:true,unique:true},
+    password :{type:String,required:true},
+    // date :{type:Date,defautlt:Date.now}
+  })
+
+
+
+  mongoose.plugin(muv)
+
+
+
+  modules.exports = mongoose.model('user', userSchema)
