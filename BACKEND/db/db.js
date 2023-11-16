a3987bd83fd4ce5aa2cb1de2ed437f68d4031e2b
Edenplan <edenplanelle@gmail.com>
2023-11-16 12:09:44 +0100

    ajout fichier db.js

--- BACKEND/db/db.js
+++ BACKEND/db/db.js
@@ -1,10 +1,11 @@
+const mongoose = require('mongoose');
 const {connect} = require ('mongoose');
 
 
 
 async function dbConnexion() {
     try {
-        await mongoose.connect('mongodb+srv://edenplanelle:<kIo2mc7uckdLiui5>@eden.n8lr2st.mongodb.net/?retryWrites=true&w=majority');
+        await mongoose.connect('mongodb+srv://edenplanelle:kIo2mc7uckdLiui5@eden.n8lr2st.mongodb.net/');
         console.log("Database connection established");
     } catch (error) {
         console.error(error);
